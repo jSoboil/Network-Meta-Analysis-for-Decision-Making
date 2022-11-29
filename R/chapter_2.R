ce7a9fa892ebf50c3fc331e23c9d0eda8b3e0b28
jSoboil <joshua@soboils.com>
2022-11-29 16:03:04 +0000

    started chapter 3

--- R/chapter_2.R
+++ R/chapter_2.R
@@ -1,7 +1,15 @@
 # Libraries ---------------------------------------------------------------
+# required packages for analysis
 pkgs <- c("R2jags", "rstan", "rstanarm", "bayesplot", "dplyr", 
           "tidyr", "igraph")
-sapply(pkgs, require, character.only = TRUE)
+# install packages if not installed
+installed_packages <- pkgs %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+ install.packages(pkgs[!installed_packages])
+}
+# load required packages
+invisible(lapply(pkgs, library, character.only = TRUE))
+# set number of parallel cores
 options(mc.cores = parallel::detectCores())
 
 # The Core Models ---------------------------------------------------------
